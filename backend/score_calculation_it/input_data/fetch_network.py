5002c5e49c46e3af53e8cb705f5650f9ddb244b3
Grandmaison Roland <rgrandmais@univ-lyon2.fr>
2026-03-02 10:59:31 +0100

    script recuperation du graphe

--- backend/score_calculation_it/input_data/fetch_network.py
+++ backend/score_calculation_it/input_data/fetch_network.py
@@ -6,31 +6,37 @@ import sys
 sys.path.append("../../")
 from global_variable import *
 
-network_filters = "[\"highway\"][\"area\"!~\"yes\"][\"highway\"!~\"abandoned|bus_guideway|construction|cycleway|motorway|trunk|planned|platform|proposed|raceway|motorway_link|trunk_link|escape|busway\"][\"foot\"!~\"no\"][\"service\"!~\"private\"][\"sidewalk\"!~\"no\"]"
-
 """
-    The bounding_metrop file contains the bounding of Lyon metropole. It is used to define graph query limit in the
+    The bounding_Villeurbanne file contains the bounding of Villeurbanne. It is used to define graph query limit in the
     ox.graph_from_polygon function.
 """
+
+def globpath(path):
+    return os.path.join(base_path, path)
+
+### BOUDING LYON METROPOLE PATH ###
+bounding_Villeurbanne_path = "C:/Users/rgrandmais/Documents/Roland/Projet_villeurbanne/boundingbox2154.gpkg"
+Vil_network_bounding_path = "C:/Users/rgrandmais/Documents/Roland/Projet_villeurbanne/network.gpkg"
+#network_filters = "[\"highway\"][\"area\"!~\"yes\"][\"highway\"!~\"abandoned|bus_guideway|construction|cycleway|motorway|trunk|planned|platform|proposed|raceway|motorway_link|trunk_link|escape|busway\"][\"foot\"!~\"no\"][\"service\"!~\"private\"][\"sidewalk\"!~\"no\"]"
 
 def fetch_OSM_graph():
     """This function load an OSM graph into the Metropole of Lyon \n
     """
-    print("Reading bouding Metrop")
-    bounding_metrop = gpd.read_file(bounding_metrop_path)
-
-    bounding_metrop = bounding_metrop.to_crs("4326")
-
-    geometry = bounding_metrop["geometry"].iloc[0]
+    print("Reading bouding Villeurbanne")
+    bounding_Vil = gpd.read_file(bounding_Villeurbanne_path)
+    bounding_Vil = bounding_Vil.to_crs("4326")
+    
+    geometry = bounding_Vil["geometry"].iloc[0]
 
     print("Fetching Graph from Lyon Metropole")
-    G = ox.graph_from_polygon(geometry, custom_filter=network_filters)
-
-    #EPSG:3946 is the default projection system used by datagrandlyon.
+    #G = ox.graph_from_polygon(geometry, custom_filter=network_filters)
+    G = ox.graph_from_polygon(geometry, network_type="walk")
+    print(bounding_Vil)
+    #EPSG:3946 is the default projection system used by Villeurbanne.
     G = ox.project_graph(G, to_crs="EPSG:3946")
 
-    print(f"Saving graph into {metrop_network_bouding_path}")
-    ox.save_graph_geopackage(G, metrop_network_bouding_path)
+    print(f"Saving graph into {Vil_network_bounding_path}")
+    ox.save_graph_geopackage(G, Vil_network_bounding_path)
 
 def bufferize(input_path, output_path, layer, buffer_size):
     """Bufferize a layer according to a buffer_size and save the ouput file"""
@@ -49,14 +55,16 @@ def bufferize(input_path, output_path, layer, buffer_size):
     layer_buffer.to_file(output_path, driver="GPKG", layer=layer)
 
 
-choice = input("Voulez vous télécharger le réseau (NETWORK) et le bufferizer (BUFFER) ou faire les deux (ALL)? \n Veuillez saisir une des 3 possibilitées : NETWORk, BUFFER, ALL : \n")
+fetch_OSM_graph()
 
-if(choice == "ALL"):
-    fetch_OSM_graph()
-    bufferize(metrop_network_bouding_path, edges_buffer_path, "edges", 6.25)
-elif(choice== "NETWORK"):
-    fetch_OSM_graph()
-elif(choice == "BUFFER"):
-    bufferize(metrop_network_bouding_path, edges_buffer_path, "edges", 6.25)
-else:
-    print("Veuillez saisir un choix valide")
+# choice = input("Voulez vous télécharger le réseau (NETWORK) et le bufferizer (BUFFER) ou faire les deux (ALL)? \n Veuillez saisir une des 3 possibilitées : NETWORk, BUFFER, ALL : \n")
+
+# if(choice == "ALL"):
+#     fetch_OSM_graph()
+#     bufferize(metrop_network_bouding_path, edges_buffer_path, "edges", 6.25)
+# elif(choice== "NETWORK"):
+#     fetch_OSM_graph()
+# elif(choice == "BUFFER"):
+#     bufferize(metrop_network_bouding_path, edges_buffer_path, "edges", 6.25)
+# else:
+#     print("Veuillez saisir un choix valide")
