91ebdafdfb09d2a9f5dd6e58aeff9cb9ea9bb544
Grandmaison Roland <rgrandmais@univ-lyon2.fr>
2026-03-06 11:01:51 +0100

    ajout des poids fraicheur (mauvais mais suffisant pour les test) et suppression du frontend

--- backend/score_calculation_it/ombre_preprocessing.py
+++ backend/score_calculation_it/ombre_preprocessing.py
@@ -15,6 +15,8 @@ sys.path.append("../")
 ###### CREATE WORKING DIRECTORY FOR OMBRES ######
 create_folder("backend/score_calculation_it/output_data/ombre/")
 shadows_08_path ="backend/score_calculation_it/input_data/ombre/8h.tif"
+shadows_13_path ="backend/score_calculation_it/input_data/ombre/13h.tif"
+shadows_18_path ="backend/score_calculation_it/input_data/ombre/18h.tif"
 edges_buffer_path = "backend/score_calculation_it/input_data/vil_network_bounding_buffer.gpkg"
 network_shadow_path = "backend/score_calculation_it/output_data/ombre/network_shadow.gpkg"
 vil_area_path = "backend/score_calculation_it/input_data/villeurbanne/villeurbanne.shp"
@@ -34,7 +36,6 @@ def calculate_shadows(): #eventuellement rajouter des paramètre pour selectionn
         transform = src.transform
         nodata = src.nodata
         raster_crs = src.crs
-
         means = []
         for geom in network_edge.geometry:
 
@@ -58,15 +59,77 @@ def calculate_shadows(): #eventuellement rajouter des paramètre pour selectionn
                 means.append(float(values.mean()))
             else:
                 means.append(np.nan)
+        network_edge["shad_8h"] = means
 
-        network_edge["portion_ombre"] = means
-    
+    with rasterio.open(shadows_13_path) as src: 
+        raster = src.read(1)
+        transform = src.transform
+        nodata = src.nodata
+        raster_crs = src.crs
+        means = []
+        for geom in network_edge.geometry:
+
+            # Rasteriser la ligne
+            mask = features.rasterize(
+                [(mapping(geom), 1)],
+                out_shape=raster.shape,
+                transform=transform,
+                fill=0,
+                dtype=np.uint8
+            )
+            
+            touched = mask == 1
+
+            if nodata is not None:
+                values = raster[(touched) & (raster != nodata)]
+            else:
+                values = raster[touched]
+
+            if len(values) > 0:
+                means.append(float(values.mean()))
+            else:
+                means.append(np.nan)
+        network_edge["shad_13h"] = means
+
+    with rasterio.open(shadows_18_path) as src: 
+        raster = src.read(1)
+        transform = src.transform
+        nodata = src.nodata
+        raster_crs = src.crs
+        means = []
+        for geom in network_edge.geometry:
+
+            # Rasteriser la ligne
+            mask = features.rasterize(
+                [(mapping(geom), 1)],
+                out_shape=raster.shape,
+                transform=transform,
+                fill=0,
+                dtype=np.uint8
+            )
+            
+            touched = mask == 1
+
+            if nodata is not None:
+                values = raster[(touched) & (raster != nodata)]
+            else:
+                values = raster[touched]
+
+            if len(values) > 0:
+                means.append(float(values.mean()))
+            else:
+                means.append(np.nan)
+        network_edge["shad_18h"] = means
 
     # Ajuster la valeur pour les segments hors Villeurbanne
     intersects_mask = network_edge.geometry.intersects(vil_area.union_all())
     non_intersect_mask = ~intersects_mask
-    network_edge.loc[non_intersect_mask, "portion_ombre"] = 0
-    network_edge["portion_ombre"]=network_edge["portion_ombre"]/255*100
+    network_edge.loc[non_intersect_mask, "shad_8h"] = 0
+    network_edge.loc[non_intersect_mask, "shad_13h"] = 0
+    network_edge.loc[non_intersect_mask, "shad_18h"] = 0
+    network_edge["shad_8h"]=network_edge["shad_8h"]/255*100
+    network_edge["shad_13h"]=network_edge["shad_13h"]/255*100
+    network_edge["shad_18h"]=network_edge["shad_18h"]/255*100
     network_edge.to_file(network_shadow_path, driver="GPKG", layer="network_shadow")
     print("fini")
     
--- backend/score_calculation_it/score_calculation.py
+++ backend/score_calculation_it/score_calculation.py
@@ -23,7 +23,9 @@ network_lineaire = gpd.read_file(network_lineaire_path, layer = "edges")
 def ponderer_all():
     fresh_value = 0
     for index, row in network_final.iterrows():
-        value_ombre = abs(row["portion_om"]*8-800)
+        value_ombre_8h = abs(row["shad_8h"]*8-800)
+        value_ombre_13h = abs(row["shad_13h"]*8-800)
+        value_ombre_18h = abs(row["shad_18h"]*8-800)
         value_brumi = abs(row["brumisateu"]*20-20)
         value_assise = abs(row["assise"]*2-2)
         value_borne_font = abs(row["borne_font"]*10-10)
@@ -43,10 +45,17 @@ def ponderer_all():
         # value_vegetation = ponderer_vegetation_present(index)
         # value_temp = ponderer_temp_present(index)
 
-        fresh_value = value_ombre+value_brumi+value_assise+value_borne_font+value_fontaine+value_parc+value_sanitaire+value_vegetation+ value_temp
-        network_final.loc[index, "fresh_value"] = fresh_value
+        fresh_8h = value_ombre_8h+value_brumi+value_assise+value_borne_font+value_fontaine+value_parc+value_sanitaire+value_vegetation+ value_temp
+        fresh_13h = value_ombre_13h+value_brumi+value_assise+value_borne_font+value_fontaine+value_parc+value_sanitaire+value_vegetation+ value_temp
+        fresh_18h = value_ombre_18h+value_brumi+value_assise+value_borne_font+value_fontaine+value_parc+value_sanitaire+value_vegetation+ value_temp
+        network_final.loc[index, "fresh_8h"] = fresh_8h
+        network_final.loc[index, "fresh_13h"] = fresh_13h
+        network_final.loc[index, "fresh_18h"] = fresh_18h
+        network_final["weight08"]= network_final["length"]*network_final["fresh_8h"]
+        network_final["weight13"]= network_final["length"]*network_final["fresh_13h"]
+        network_final["weight18"]= network_final["length"]*network_final["fresh_18h"]
     
-    col_freshness = ["u", "v", "fresh_value"] 
+    col_freshness = ["u", "v", "fresh_8h", "fresh_13h", "fresh_18h","weight08","weight13","weight18"] 
     col_freshness_subset = network_final[col_freshness]
 
     network_lineaire_final = network_lineaire.merge(
@@ -56,6 +65,8 @@ def ponderer_all():
         right_on=["u","v"]
     )
     
+    
+
     network_lineaire_final.to_file(network_final_path, driver="ESRI Shapefile")
 
 ponderer_all()
